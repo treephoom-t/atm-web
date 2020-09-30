ff2173764fe95a46b87bba964f2e2b0cf5635a6b
treephoom-t <36246070+treephoom-t@users.noreply.github.com>
2020-09-30 10:18:05 +0700

    add post methods to bank account

--- src/main/java/th/ac/ku/atm/controller/BankAccountController.java
+++ src/main/java/th/ac/ku/atm/controller/BankAccountController.java
@@ -1,7 +1,12 @@
 package th.ac.ku.atm.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import th.ac.ku.atm.model.BankAccount;
 import th.ac.ku.atm.service.BankAccountService;
 
 @Controller
@@ -11,6 +16,19 @@ public class BankAccountController {
 
     public BankAccountController(BankAccountService bankAccountService) {
         this.bankAccountService = bankAccountService;
+    }
+
+    @GetMapping
+    public String getBankAccountPage(Model model) {
+        model.addAttribute("bankaccounts", bankAccountService.getBankAccounts());
+        return "bankaccount";
+    }
+
+    @PostMapping
+    public String openAccount(@ModelAttribute BankAccount bankAccount, Model model) {
+        bankAccountService.openAccount(bankAccount);
+        model.addAttribute("bankaccounts", bankAccountService.getBankAccounts());
+        return "redirect:bankaccount";
     }
 }
 
